cc9640d7cc660adaff38053c3c57293f221566ae
HieuNguyeen <hn151331@gmail.com>
2025-02-13 14:33:22 +0700

    -create admin and user migrations -connect to mysql database -configure MAIL sending functionality (mailtrap)

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,11 +13,17 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->nullable();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('photo')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('phone')->nullable();
+            $table->string('country')->nullable();
+            $table->string('city')->nullable();
+            $table->string('address')->nullable();
+            $table->string('zip')->nullable();
+            $table->string('token')->nullable();
+            $table->string('status')->default(0)->comment('0: pending, 1: active, 2: suspended');
             $table->timestamps();
         });
 
