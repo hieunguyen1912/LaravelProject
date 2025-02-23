788ef9e87594ad7efb072169b64296435d2ea12d
HieuNguyeen <hn151331@gmail.com>
2025-02-23 20:02:43 +0700

    create edit profile for admin, make forget reset password function

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,19 @@ Route::get('/', function () {
 
 Route::middleware('admin')->prefix('admin')->group(function() {
     Route::get('/profile', [AdminAuthController::class, 'profile'])->name('admin_profile');
+    Route::post('/profile', [AdminAuthController::class, 'profile_submit'])->name('admin_profile_submit');
+
     Route::get('/dashboard', [AdminDashboardController::class, 'Dashboard'])->name('admin_dashboard');
 });
 Route::prefix('admin')->group(function() {
     Route::get('/login', [AdminAuthController::class, 'login'])->name('admin_login');
     Route::post('/login', [AdminAuthController::class, 'login_submit'])->name('admin_login_submit');
+
     Route::get('/forget-password', [AdminAuthController::class, 'forgetPassword'])->name('admin_forget_password');
-    Route::get('/reset-password', [AdminAuthController::class, 'resetPassword'])->name('admin_reset_password');
+    Route::post('/forget-password', [AdminAuthController::class, 'forget_password_submit'])->name('admin_forget_password_submit');
+
+    Route::get('/reset-password/{token}/{email}', [AdminAuthController::class, 'resetPassword'])->name('admin_reset_password');
+    Route::post('/reset-password/{token}/{email}', [AdminAuthController::class, 'reset_password_submit'])->name('admin_reset_password_submit');
+    
     Route::get('/logout', [AdminAuthController::class, 'logout'])->name('admin_logout');
 });
